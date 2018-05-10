c836684126468258dfba85e6656c29c416114392
Sean Blair <seandibango@gmail.com>
2018-05-10 10:38:15 -0700

    Add a barbones static html example of app input and look

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import './App.css';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
 function App() {
   return (
-    <div>
-      <h1>My Five Minute Journal </h1>
-      <h2>Today I am grateful for</h2>
-      <p>Answer:</p>
+    <div className="page">
+      <h1 className="title">My Five Minute Journal </h1>
+      <div className="question">
+        <h2>Today I am grateful for:</h2>
+        <p>1)</p>
+        <p>2)</p>
+        <p>3)</p>
+      </div>
+      <hr />
+      <div className="answers">
+        <div className="answer">
+          <h2>Yesterday I was grateful for:</h2>
+          <p>Emily :)</p>
+          <p>Canada</p>
+          <p>EI</p>
+        </div>
+        <div className="answer">
+          <h2>What would make yesterday great was:</h2>
+          <p>Finish resume</p>
+          <p>Start five minute journal React app</p>
+          <p>Do a tweaked out rock to fakie.</p>
+        </div>
+        <div className="answer">
+          <h2>My affirmations yesterday where:</h2>
+          <p>I am reasonably smart</p>
+          <p>I am a hard worker</p>
+          <p>I have intentions of being kind</p>
+        </div>
+      </div>
     </div>
   );
 }
