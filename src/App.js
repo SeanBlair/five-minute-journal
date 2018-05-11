1c8ed388cd59bef6e2c1c39c10406925dd585c18
Sean Blair <seandibango@gmail.com>
2018-05-10 18:44:57 -0700

    Add some comments describing ideas to handle the app's state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import JOURNAL_ENTRIES from './journalEntriesData';
 import JournalEntry from './JournalEntry';
 
 function App() {
+  // app will have the state because it gets modified from Question
+  // and needs to update the journal entries div.
+  // the state will get loaded from HTML local storage on page load...
+  // on update both the local storage and the state will get modified.
+  // App will know which is the next Question to display.
+
   return (
     <div className="page">
       <h1 className="title">My Five Minute Journal </h1>
