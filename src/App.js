6f6662a60a1bffba3f4533444c308d93be675587
Sean Blair <seandibango@gmail.com>
2018-05-13 16:28:22 -0700

    Add some TODO comments

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ class App extends React.Component {
   // sections slightly different?
 
   // TODO: Refactor for clarity and maintainability!!!
-  // TODO: Add button to clear all journal entries.
+  // TODO: Add button to clear all journal entries from localStorage.
+  // TODO: Fix answers border-radius when entry not full.
+  // TODO: Consider rethinking state + stored data to minimize moving parts.
+  // Re read https://reactjs.org/docs/thinking-in-react.html  prefer
+
+  // TODO: focus on first answer of new question on submit.
 
   constructor(props) {
     super(props);
@@ -23,17 +28,22 @@ class App extends React.Component {
       journalQuestions.amazing,
       journalQuestions.better
     ];
-    this.currentQuestion = undefined;
+    // TODO: probably do not need this field as the info will always be
+    // available and correct in localStorage.getItem('historicJournalEntries').currentQuestionNum
+    // but don't want to query localStorage for next question num!!!
+    this.currentQuestionIndex = 0;
+    // Get journal state prior to this page load.
     let historicJournalEntries = localStorage.getItem('historicJournalEntries');
+    // No previous journal entries saved.
     if (!historicJournalEntries) {
       historicJournalEntries = { entries: [] };
-      this.currentQuestion = 0;
     } else {
       historicJournalEntries = JSON.parse(historicJournalEntries);
-      this.currentQuestion = historicJournalEntries.currentQuestionNum;
+      this.currentQuestionIndex = historicJournalEntries.currentQuestionNum;
     }
+
     this.state = {
-      question: this.questions[this.currentQuestion],
+      question: this.questions[this.currentQuestionIndex],
       journalEntries: historicJournalEntries.entries
     };
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -50,45 +60,55 @@ class App extends React.Component {
   }
 
   handleSubmit() {
-    if (this.currentQuestion === this.questions.length - 1) {
-      this.currentQuestion = 0;
+    if (this.currentQuestionIndex === this.questions.length - 1) {
+      this.currentQuestionIndex = 0;
     } else {
-      this.currentQuestion++;
+      this.currentQuestionIndex++;
     }
     const today = new Date().toDateString();
     let newEntries;
     const question = this.state.question;
+    // answers to add
     const answers = [question.answers.first, question.answers.second];
     if (question.answers.third) {
       answers.push(question.answers.third);
     }
 
+    // Not first journal entry...  Is this necessary??
     if (this.state.journalEntries.length > 0) {
       const firstEntriesDate = this.state.journalEntries[0].date;
+      // Today's entry already started.
       if (today === firstEntriesDate) {
+        // this can be avoided if initialized with both nightEntry and morningEntry
         if (!this.state.journalEntries[0].hasOwnProperty(question.type)) {
           newEntries = update(this.state.journalEntries, {
             0: { [question.type]: { $set: {} } }
           });
+          // add new question + answers
           newEntries = update(newEntries, {
             0: { [question.type]: { [question.name]: { $set: answers } } }
           });
+          // see above to eliminate this duplicate code.
         } else {
           newEntries = update(this.state.journalEntries, {
             0: { [question.type]: { [question.name]: { $set: answers } } }
           });
         }
+        // first entry for Today
       } else {
+        // TODO: initialize both questionType objects to avoid extra logic above
         const entry = {
           date: today,
           [question.type]: {
             [question.name]: answers
           }
         };
+        // add new entry to journalEntries
         newEntries = update(this.state.journalEntries, {
           0: { $unshift: [entry] }
         });
       }
+      // this seems unnecessary
     } else {
       newEntries = [
         {
@@ -100,11 +120,12 @@ class App extends React.Component {
       ];
     }
     this.setState({
-      question: this.questions[this.currentQuestion],
+      question: this.questions[this.currentQuestionIndex],
       journalEntries: newEntries
     });
+    // prepare to update entries in localStorage
     const storedEntries = {
-      currentQuestionNum: this.currentQuestion,
+      currentQuestionNum: this.currentQuestionIndex,
       entries: newEntries
     };
     localStorage.setItem(
